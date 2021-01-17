4fc4a4d1484478f17a9305fc397d902139f7f858
Ramsey Dow <yesmar@gmail.com>
2021-01-17 13:59:38 -0800

    Add OOP58-CPP rule

---
+++ rules/oop/58/nc0.cpp
@@ -0,0 +1,31 @@
+// OOP58-CPP: Noncompliant Code Example
+#include <algorithm>
+#include <vector>
+ 
+class A {
+  mutable int m;
+   
+public:
+  A() : m(0) {}
+  explicit A(int m) : m(m) {}
+   
+  A(const A &other) : m(other.m) {
+    other.m = 0;
+  }
+   
+  A& operator=(const A &other) {
+    if (&other != this) {
+      m = other.m;
+      other.m = 0;
+    }
+    return *this;
+  }
+   
+  int get_m() const { return m; }
+};
+ 
+void f() {
+  std::vector<A> v{10};
+  A obj(12);
+  std::fill(v.begin(), v.end(), obj);
+}
