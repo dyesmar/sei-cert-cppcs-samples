d0449d6d6daa3ddea4165c1e4f69342d5885ce06
Ramsey Dow <yesmar@gmail.com>
2021-01-17 12:39:23 -0800

    Add FIO50-CPP rule

---
+++ rules/fio/50/c0.cpp
@@ -0,0 +1,17 @@
+// FIO50-CPP: Compliant Solution
+#include <fstream>
+#include <string>
+ 
+void f(const std::string &fileName) {
+  std::fstream file(fileName);
+  if (!file.is_open()) {
+    // Handle error
+    return;
+  }
+   
+  file << "Output some data";
+  
+  std::string str;
+  file.seekg(0, std::ios::beg);
+  file >> str;
+}
---
+++ rules/fio/50/nc0.cpp
@@ -0,0 +1,15 @@
+// FIO50-CPP: Noncompliant Code Example
+#include <fstream>
+#include <string>
+ 
+void f(const std::string &fileName) {
+  std::fstream file(fileName);
+  if (!file.is_open()) {
+    // Handle error
+    return;
+  }
+   
+  file << "Output some data";
+  std::string str;
+  file >> str;
+}
